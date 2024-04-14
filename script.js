3b3df0fc0bbd6476df53a061a2006ed9eded8ded
algoritmicheskie <algoritmikaobs@gmail.com>
2024-04-14 13:36:18 +0300

    kek

---
+++ script.js
@@ -0,0 +1,26 @@
+let cookie = document.querySelector(".cookie")
+let scoreHTML = document.querySelector(".score")
+
+let score = 0
+
+cookie.addEventListener("click", function(){
+    score = score + 1
+    scoreHTML.innerHTML = score
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
